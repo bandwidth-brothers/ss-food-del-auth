91c2e39a80a59e672163f46f3715d0fb608531ca
brunoara21 <bruno.rebaza@smoothstack.com>
2021-08-25 13:46:10 -0400

    BB-12 feat:user-register

---
+++ src/main/java/com/smoothstack/scrumptious_auth/ScrumptiousAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.smoothstack.scrumptious_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrumptiousAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrumptiousAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smoothstack/scrumptious_auth/ScrumptiousAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smoothstack.scrumptious_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumptiousAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
