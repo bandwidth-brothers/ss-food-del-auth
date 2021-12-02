f7cbf4b5eb366c9ce53f7ff1237fc16b2fdec11c
johnzhengss <87684806+johnzhengss@users.noreply.github.com>
2021-12-02 10:34:09 -0600

    Update SecurityConfig.java

--- src/main/java/com/ss/scrumptious_auth/security/SecurityConfig.java
+++ src/main/java/com/ss/scrumptious_auth/security/SecurityConfig.java
@@ -79,13 +79,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     ));
     }
 
-//    @Bean
-//    CorsConfigurationSource corsConfigurationSource() {
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
-//        return source;
-//    }
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
 
 
 
