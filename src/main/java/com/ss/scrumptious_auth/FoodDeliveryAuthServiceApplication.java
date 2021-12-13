d552bc65ff381e423703acf68c99b2b597792959
brunoara21 <bruno.rebaza@smoothstack.com>
2021-12-13 12:53:05 -0500

    Remove Eureka

--- src/main/java/com/ss/scrumptious_auth/FoodDeliveryAuthServiceApplication.java
+++ src/main/java/com/ss/scrumptious_auth/FoodDeliveryAuthServiceApplication.java
@@ -2,15 +2,12 @@ package com.ss.scrumptious_auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 public class FoodDeliveryAuthServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FoodDeliveryAuthServiceApplication.class, args);
     }
-
 }
 	
